56cdaa12fd33d4d948a5d4aef441f0a7d1420ced
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-19 20:51:13 +0530

    [CACHE] Regenerated demo for cache_cache_maintenance_sam_rh71_ek
    MH3-68292
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/cache/cache_maintenance/firmware/src/packs/ATSAMRH71F20C_DFP/instance/tc0.h
+++ apps/cache/cache_maintenance/firmware/src/packs/ATSAMRH71F20C_DFP/instance/tc0.h
@@ -1,7 +1,7 @@
-/**
- * \brief Instance header file for ATSAMRH71F20C
+/*
+ * Instance header file for ATSAMRH71F20C
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,33 +20,33 @@
  *
  */
 
-/* file generated from device description version 2021-05-04T16:48:28Z */
+/* file generated from device description version 2022-01-06T10:24:17Z */
 #ifndef _SAMRH71_TC0_INSTANCE_
 #define _SAMRH71_TC0_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for TC0 peripheral ========== */
-#define TC0_DMAC_ID_RX                           _UL_(24)   
-#define TC0_DMAC_ID_CPA                          _UL_(29)   
-#define TC0_DMAC_ID_CPB                          _UL_(33)   
-#define TC0_DMAC_ID_CPC                          _UL_(37)   
-#define TC0_DMAC_ID_ETRG                         _UL_(41)   
-#define TC0_INSTANCE_ID_CHANNEL0                 _UL_(25)   
-#define TC0_INSTANCE_ID_CHANNEL1                 _UL_(26)   
-#define TC0_INSTANCE_ID_CHANNEL2                 _UL_(27)   
-#define TC0_CLOCK_ID_CHANNEL0                    _UL_(25)   
-#define TC0_CLOCK_ID_CHANNEL1                    _UL_(26)   
-#define TC0_CLOCK_ID_CHANNEL2                    _UL_(27)   
-#define TC0_TCCLKS_                              _UL_(0)    /* MCK */
-#define TC0_TCCLKS_TIMER_CLOCK1                  _UL_(1)    /* GCLK */
-#define TC0_TCCLKS_TIMER_CLOCK2                  _UL_(2)    /* MCK/8 */
-#define TC0_TCCLKS_TIMER_CLOCK3                  _UL_(3)    /* MCK/32 */
-#define TC0_TCCLKS_TIMER_CLOCK4                  _UL_(4)    /* MCK/128 */
-#define TC0_TCCLKS_TIMER_CLOCK5                  _UL_(5)    /* SLCK */
-#define TC0_TCCLKS_XC0                           _UL_(6)    /* XC0 */
-#define TC0_TCCLKS_XC1                           _UL_(7)    /* XC1 */
-#define TC0_TCCLKS_XC2                           _UL_(8)    /* XC2 */
-#define TC0_NUM_INTERRUPT_LINES                  _UL_(3)    
-#define TC0_TIMER_WIDTH                          _UL_(32)   
+#define TC0_DMAC_ID_RX                           (24)       
+#define TC0_DMAC_ID_CPA                          (29)       
+#define TC0_DMAC_ID_CPB                          (33)       
+#define TC0_DMAC_ID_CPC                          (37)       
+#define TC0_DMAC_ID_ETRG                         (41)       
+#define TC0_INSTANCE_ID_CHANNEL0                 (25)       
+#define TC0_INSTANCE_ID_CHANNEL1                 (26)       
+#define TC0_INSTANCE_ID_CHANNEL2                 (27)       
+#define TC0_CLOCK_ID_CHANNEL0                    (25)       
+#define TC0_CLOCK_ID_CHANNEL1                    (26)       
+#define TC0_CLOCK_ID_CHANNEL2                    (27)       
+#define TC0_TCCLKS_                              (0)        /* MCK */
+#define TC0_TCCLKS_TIMER_CLOCK1                  (1)        /* GCLK */
+#define TC0_TCCLKS_TIMER_CLOCK2                  (2)        /* MCK/8 */
+#define TC0_TCCLKS_TIMER_CLOCK3                  (3)        /* MCK/32 */
+#define TC0_TCCLKS_TIMER_CLOCK4                  (4)        /* MCK/128 */
+#define TC0_TCCLKS_TIMER_CLOCK5                  (5)        /* SLCK */
+#define TC0_TCCLKS_XC0                           (6)        /* XC0 */
+#define TC0_TCCLKS_XC1                           (7)        /* XC1 */
+#define TC0_TCCLKS_XC2                           (8)        /* XC2 */
+#define TC0_NUM_INTERRUPT_LINES                  (3)        
+#define TC0_TIMER_WIDTH                          (32)       
 
 #endif /* _SAMRH71_TC0_INSTANCE_ */
