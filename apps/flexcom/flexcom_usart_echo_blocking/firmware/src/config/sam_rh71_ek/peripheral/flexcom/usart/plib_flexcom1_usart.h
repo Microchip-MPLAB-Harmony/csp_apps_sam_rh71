9f8fee22c5bbbcd6b09c9e60a4c8c71df7bf6a0c
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-19 21:00:30 +0530

    [FLEXCOM] Regenerated demo for flexcom_flexcom_usart_echo_blocking_sam_rh71_ek
    MH3-68296
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/flexcom/flexcom_usart_echo_blocking/firmware/src/config/sam_rh71_ek/peripheral/flexcom/usart/plib_flexcom1_usart.h
+++ apps/flexcom/flexcom_usart_echo_blocking/firmware/src/config/sam_rh71_ek/peripheral/flexcom/usart/plib_flexcom1_usart.h
@@ -88,6 +88,8 @@ bool FLEXCOM1_USART_TransmitterIsReady( void );
 bool FLEXCOM1_USART_ReceiverIsReady( void );
 
 
+bool FLEXCOM1_USART_TransmitComplete( void );
+
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
