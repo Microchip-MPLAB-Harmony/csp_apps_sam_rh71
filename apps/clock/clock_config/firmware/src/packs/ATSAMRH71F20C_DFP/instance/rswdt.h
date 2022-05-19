192dddc1bd09eb316196915a20f41c26d2656883
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-19 20:53:50 +0530

    [CLOCK] Regenerated demo for clock_clock_config_sam_rh71_ek
    MH3-68293
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/clock/clock_config/firmware/src/packs/ATSAMRH71F20C_DFP/instance/rswdt.h
+++ apps/clock/clock_config/firmware/src/packs/ATSAMRH71F20C_DFP/instance/rswdt.h
@@ -1,7 +1,7 @@
-/**
- * \brief Instance header file for ATSAMRH71F20C
+/*
+ * Instance header file for ATSAMRH71F20C
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,12 +20,12 @@
  *
  */
 
-/* file generated from device description version 2021-05-04T16:48:28Z */
+/* file generated from device description version 2022-01-06T10:24:17Z */
 #ifndef _SAMRH71_RSWDT_INSTANCE_
 #define _SAMRH71_RSWDT_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for RSWDT peripheral ========== */
-#define RSWDT_INSTANCE_ID                        _UL_(47)   
+#define RSWDT_INSTANCE_ID                        (47)       
 
 #endif /* _SAMRH71_RSWDT_INSTANCE_ */
